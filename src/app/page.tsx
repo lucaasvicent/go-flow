a5a135bc40eb3aab82958b83503c160fd4584fda
Lucas Vicente <lucasdev08@hotmail.com>
2025-08-28 18:58:36 -0300

    fix(folders): correction in name the folders and importations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
-import About from "./components/sections/about/About";
-import FAQ from "./components/sections/faq/FAQ";
-import Hero from "./components/sections/hero/hero";
-import Solutions from "./components/sections/solutions/Solutions";
+import About from "./components/sections/About";
+import FAQ from "./components/sections/FAQ";
+import Hero from "./components/sections/Hero";
+import Solutions from "./components/sections/Solutions";
 
 export default function Home() {
   return (
